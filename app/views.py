f6f127d4f45f901a76f63635fb1905f4af1bfd58
Cristiano <cristiano.souzac1@gmail.com>
2020-07-29 20:58:28 -0300

    Usando Generics Class Base View

--- app/views.py
+++ app/views.py
@@ -1,49 +1,13 @@
 from app.models import Todo
 from app.serializers import TodoSerializer
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
-from rest_framework.views import APIView
-from rest_framework.exceptions import NotFound
+from rest_framework import generics
 
 
-class TodoListAndCreate(APIView):
-    def get(self, request):
-        todo = Todo.objects.all()
-        serializer = TodoSerializer(todo, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = TodoSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class TodoListAndCreate(generics.ListCreateAPIView):  # Classe já imprementa todos os metodos automaticamente.
+   queryset = Todo.objects.all()  # Filtro banco de dados
+   serializer_class = TodoSerializer
 
 
-class TodoDetailChangeAndDelete(APIView):
-
-    def get_object(self, pk):
-        try:
-            return Todo.objects.get(pk=pk)
-        except Todo.DoesNotExist:
-            raise NotFound()
-
-    def get(self, request, pk):
-        todo = self.get_object(pk)
-        serializer = TodoSerializer(todo)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        todo = self.get_object(pk)
-        serializer = TodoSerializer(todo, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        todo = self.get_object(pk)
-        todo.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class TodoDetailChangeAndDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Todo.objects.all()  # Filtro banco de dados
+    serializer_class = TodoSerializer
